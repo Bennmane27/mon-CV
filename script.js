64251fe9689f1275dec55cf5218bff50494f2bd3
Bennani Othmane <86740046+Bennmane27@users.noreply.github.com>
2024-05-23 22:25:11 +0200

    correction Bug js hamburger menu

--- script.js
+++ script.js
@@ -52,18 +52,4 @@ document.addEventListener('DOMContentLoaded', function () {
     navToggle.addEventListener('click', () => {
         navLinksContainer.classList.toggle('active');
     });
-
-    // Back to Top Button
-    const backToTopButton = document.getElementById('back-to-top');
-    window.addEventListener('scroll', () => {
-        if (window.pageYOffset > 300) {
-            backToTopButton.classList.add('visible');
-        } else {
-            backToTopButton.classList.remove('visible');
-        }
-    });
-
-    backToTopButton.addEventListener('click', () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
 });
